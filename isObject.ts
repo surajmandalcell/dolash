92a02a4ac96a1686eb87c0a474619dd0f289f4fc
Suraj Mandal <surajmandalcell@gmail.com>
2021-03-19 10:43:24 +0530

    initial two functions

---
+++ isObject.ts
@@ -0,0 +1,16 @@
+/**
+ * ### Checks if passed value is type of object(not default javascript typeof)
+ *
+ * Cases handled, checks for null & array seperately(typeof error)
+ *
+ * @param err any type of data/variable
+ *
+ * Sign: SurajMandal
+ *
+ * Website: surajmandal.in
+ *
+ * ts: true
+ */
+export function isObject(prop: any): boolean {
+  return prop !== null && typeof prop === 'object' && !Array.isArray(prop);
+}
